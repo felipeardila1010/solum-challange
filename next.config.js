221b723af2a1bc01a122865bb5f2423533506a80
felipeardila1010 <felipeardila1010@hotmail.com>
2025-05-14 01:38:07 -0500

    ci: adding new general host for my back

--- next.config.js
+++ next.config.js
@@ -7,21 +7,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            : "https://solum-challange.vercel.app/api/",
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            : "https://solum-challange.vercel.app/api/docs",
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            : "https://solum-challange.vercel.app/api/openapi.json",
       },
     ];
   },
